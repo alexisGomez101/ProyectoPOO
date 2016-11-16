a0cad94c7efc744a66c41aabab8973be0e022ad7
alexisGomez101 <alexis.gomeznh@gmail.com>
2016-11-16 17:41:11 -0600

    Add files via upload

--- index_admin.php
+++ index_admin.php
@@ -170,6 +170,7 @@
     <script src="js/plugins/morris/raphael.min.js"></script>
     <script src="js/plugins/morris/morris.min.js"></script>
     <script src="js/plugins/morris/morris-data.js"></script>
+    <script src="js/controlador_admin2.js"></script>
 
 </body>
 
