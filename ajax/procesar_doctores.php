ce2db45fd8e44b3dc961fb6a900d4c00de31beb1
alexisGomez101 <alexis.gomeznh@gmail.com>
2016-11-17 11:45:57 -0600

    Add files via upload

--- ajax/procesar_doctores.php
+++ ajax/procesar_doctores.php
@@ -1,63 +1,92 @@
 <?php 
+
 switch ($_GET["accion"]) {
 	case '1':
-		$nombre = $_POST["txt-nombre"];
+	    include_once("../class/class_conexion.php");
+		include_once("../class/class_expediente.php");
+
+		$conexion = new Conexion();
+       $registro = new registrar_medicamento(null,
+            $_POST["txt-peso"],$_POST["txt-altura"],$_POST["txt-temperatura"],$_POST["txt-sintomas"],$_POST["txt-fecha-cita"],$_POST["txt-hora-cita"]
+            );
+
+		$peso = $_POST["txt-peso"];
 	    $numeroExpediente = $_POST["txt-noExpediente"];
 	    $fecha = $_POST["txt-fecha"];
 	    $peso =$_POST["txt-peso"];
 	    $edad =$_POST["txt-edad"];
 	    $descripcion =$_POST["txt-descripcion"];
 
-	    if (strlen($fecha)==0||strlen($nombre)==0||strlen($numeroExpediente)==0){       
-	    	if(strlen($edad)==0||strlen($peso)==0||strlen($descripcion)==0) {
-	            echo "Y";
-	        }else{
-	            echo "N";
-	        }
-	    }else{
-	        echo "N";  
-	    }
+	    
 
+	  $registro -> guardarRegistro($conexion);
 
 		break;
 
 	case '2':
-		echo "bussqueda";
+			include_once("../class/class_conexion.php");		
+		    
+            $conexion = new Conexion();
+          
+            $resultado=$conexion->EjecutarInstruccion(
+                'SELECT a.id_expediente, a.sintomas,a.cod_Paciente,b.temperatura,
+			 b.peso,c.nombre, c.apellido
+			FROM tbl_expediente a
+			INNER JOIN tbl_preclinica b
+			INNER JOIN tbl_datospaciente c
+			On b.id_expediente='.$_POST["txt-codigo"]);
+
+			$fila = $conexion->obtenerFila($resultado)
+
+         ?>
+         <table class = "table table-striped table-hover">
+         <td>Nombre:</td>
+          <td>
+          <input type="text" id="txt-nombre" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["nombre"].'"';?> ></td>
+   
+          <tr>
+          <td>Peso:</td>
+          <td>
+          <input type="text" id="txt-nombre" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["peso"].'"';?> ></td>
+          </tr>
+
+          <tr>
+          <td>Sintomas:</td>
+          <td>
+          <input type="text" id="txt-nombre" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["sintomas"].'"';?> ></td>
+          </tr>
+          
+          </table>
+
+   <?php
 		break;
 	
 
 	case '3':
-		$codigo = $_POST["txt-codigo"];
-		$nombre = $_POST["txt-nombre"];
-		$observaciones = $_POST["txt-observaciones"];
-		$edad = $_POST["txt-edad"];
 		$peso = $_POST["txt-peso"];
 		$altura = $_POST["txt-altura"];
 		$temperatura = $_POST["txt-temperatura"];
-		$presion = $_POST["txt-presion"];
-		$sintomas = $_POST["txt-sintomas"];
-		$especificaciones = $_POST["txt-especificaciones"];
-		$fechaCita = $_POST["txt-fecha-cita"];
+		$sintomas = $_POST["slc-sintomas"];
+		$fecha = $_POST["txt-fecha-cita"];
+		$hora= $_POST["txt-hora-cita"];
 
-
-		if(strlen($codigo)==0||strlen($nombre)==0||strlen($observaciones)==0){
-			if(strlen($edad)==0||strlen($peso)==0||strlen($altura)==0){
-				if(strlen($temperatura)==0||strlen($presion)==0||strlen($sintomas)==0){
-					if(strlen($especificaciones)==0||strlen($fechaCita)==0){
-						echo "Y";
-					}else{
-						echo "N";
-					}
+		if(strlen($peso)==0||strlen($altura==0||strlen($temperatura)==0){
+			if(strlen($sintomas)==0){
+				if(strlen($fecha)==0||strlen($hora)==0){
+					echo "Y";
 				}else{
-					echo "N";
-				}
-			}else{
+				echo "N";
+			}
+			}else {
 				echo "N";
 			}
 		}else{
-				echo "N";
+			echo "N";
 		}
+
+
 		break;
+		
 	
 	default:
 		echo "Error";
