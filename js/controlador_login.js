2adf57a202c2dae2f82c83264558813e846f131a
alexisGomez101 <alexis.gomeznh@gmail.com>
2016-11-17 02:13:53 -0600

    Add files via upload

--- js/controlador_login.js
+++ js/controlador_login.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
-
 	$("#btn-ingresar").click(function(){
+			var parametros = "txt-codigo"+$("#txt-codigo").val() + 
+						"&txt-contrasena="+$("#txt-contrasena").val();
+			alert(parametros);
+			$.ajax({
+				url:"ajax/acciones_login.php?accion=1",
+				method: "POST",
+				data: parametros,
+				dataType: 'json',
+				success:function(respuesta){
+					$("#resultado").html(respuesta.resultado + "Tipo Usuario: " + respuesta.cod_tipo_usuario);
+					if(respuesta.cod_tipo_usuario =='1')
+						window.location="index_admin.php";
+					else if(respuesta.cod_tipo_usuario =='2')
+						window.location="index_enfermeros.php";
+					else if(respuesta.cod_tipo_usuario =='3')
+						window.location="index_doctor.php";
+					else if(respuesta.cod_tipo_usuario =='4')
+						window.location="index_farmacia.php";
+				},
 
-		$.ajax({
-			url:"ajax/acciones_login.php",
-			method:"POST",
-			success: function(respuesta){
-			alert(respuesta);
-		},
-		error: function(){
+				error:function(){
 
-		}
+				}
 		});
+	});	
+});
 
-	});
-})
