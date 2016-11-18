be8cc22a88f4c9e51451859991a00a54c0f34de7
JulianLicona <jlicona_95@hotmail.com>
2016-11-18 02:00:56 -0600

    Add files via upload

--- js/controlador_farmacia.js
+++ js/controlador_farmacia.js
@@ -4,9 +4,80 @@ function Validacion(){
     
                 var parametros = "txt-nombre= "+$("#txt-nombre").val()+"&"+
                                 "txt-cantidad="+$("#txt-cantidad").val()+"&"+
-                                "txt-descripcion="+$("#txt-descripcion").val()+"&"+
+                                "txt-precio="+$("#txt-precio").val()+"&"+
                                 "txt-fechaElaboracion="+$("#txt-fechaElaboracion").val()+"&"+
-                                "txt-fechaVencimiento="+$("#txt-fechaVencimiento").val(); 
+                                "txt-fechaVencimiento="+$("#txt-fechaVencimiento").val()+"&"+
+                                "txt-sucursal="+$("#txt-sucursal").val();
+       
+
+                $.ajax({
+                    url:"../../ajax/procesar_farmacia.php?accion=1",
+                    method:"POST",
+                    data: parametros,
+                    success:function(respuesta){
+                        
+                        if(respuesta=="Y")
+                            {
+                                alert("Debe llenar todos los campos");
+
+                                if($("#txt-nombre").val()==""){
+                                    $("#txt-nombre").css("background-color","F78181");
+                                }else {
+                                    $("#txt-nombre").css("background-color","fff");
+                                }
+
+                                if($("#txt-cantidad").val()==""){
+                                    $("#txt-cantidad").css("background-color","F78181");
+                                }else {
+                                    $("#txt-cantidad").css("background-color","fff");
+                                }
+
+                                if($("#txt-fechaElaboracion").val()==""){
+                                    $("#txt-fechaElaboracion").css("background-color","F78181");
+                                }else {
+                                    $("#txt-fechaElaboracion").css("background-color","fff");
+                                }
+                                if($("#txt-fechaVencimiento").val()==""){
+                                    $("#txt-fechaVencimiento").css("background-color","F78181");
+                                }else {
+                                    $("#txt-fechaVencimiento").css("background-color","fff");
+                                }
+                                if($("#txt-precio").val()==""){
+                                    $("#txt-precio").css("background-color","F78181");
+                                }else {
+                                    $("#txt-precio").css("background-color","fff");
+                                }
+
+
+                        }else {
+                            alert("Campos llenos");
+                            $("#txt-nombre").css("background-color","fff");
+                            $("#txt-cantidad").css("background-color","fff");
+                            $("#txt-precio").css("background-color","fff");
+                            $("#txt-fechaElaboracion").css("background-color","fff");
+                            $("#txt-fechaVencimiento").css("background-color","fff");
+                            Registrar();
+                            }
+                 },
+                    error:function(){
+                        alert("Ocurrio un error.");
+                    }
+                }); 
+            
+}
+
+//===================================================================================================================
+
+ function Validacion2(){
+
+    
+                var parametros = "txt-nombre= "+$("#txt-nombre").val()+"&"+
+                                "txt-cantidad="+$("#txt-cantidad").val()+"&"+
+                                "txt-precio="+$("#txt-precio").val()+"&"+
+                                "txt-fechaElaboracion="+$("#txt-fechaElaboracion").val()+"&"+
+                                "txt-fechaVencimiento="+$("#txt-fechaVencimiento").val()+"&"+
+                                "txt-sucursal="+$("#txt-sucursal").val();
+                          
 
 
                 $.ajax({
@@ -30,11 +101,7 @@ function Validacion(){
                                 }else {
                                     $("#txt-cantidad").css("background-color","fff");
                                 }
-                                if($("#txt-descripcion").val()==""){
-                                    $("#txt-descripcion").css("background-color","F78181");
-                                }else {
-                                    $("#txt-descripcion").css("background-color","fff");
-                                }
+
                                 if($("#txt-fechaElaboracion").val()==""){
                                     $("#txt-fechaElaboracion").css("background-color","F78181");
                                 }else {
@@ -45,16 +112,21 @@ function Validacion(){
                                 }else {
                                     $("#txt-fechaVencimiento").css("background-color","fff");
                                 }
+                                if($("#txt-precio").val()==""){
+                                    $("#txt-precio").css("background-color","F78181");
+                                }else {
+                                    $("#txt-precio").css("background-color","fff");
+                                }
 
 
                         }else {
                             alert("Campos llenos");
                             $("#txt-nombre").css("background-color","fff");
                             $("#txt-cantidad").css("background-color","fff");
-                            $("#txt-descripcion").css("background-color","fff");
+                            $("#txt-precio").css("background-color","fff");
                             $("#txt-fechaElaboracion").css("background-color","fff");
                             $("#txt-fechaVencimiento").css("background-color","fff");
-                            Registrar();
+                            Actualizar();
                             }
                  },
                     error:function(){
@@ -64,34 +136,58 @@ function Validacion(){
             
 }
 
+//================================================================================================================
 
 
-function busqueda(){
 
-    var parametros = "txt-busqueda="+$("#txt-busqueda").val();
+function busqueda(codigoMedicamento){
 
             $.ajax({
                 url:"../../ajax/procesar_farmacia.php?accion=7",
+                data:"Codigo_Medicamento="+codigoMedicamento,
                 method: "POST",
-                data: parametros,
+                dataType: 'json',
                 success: function(respuesta){
-                    alert(respuesta);
+                    if(respuesta == null){
+                        alert("Medicamento no registrado");
+                    }
+                        $("#txt-nombre").val(respuesta.nombre);
+                        $("#txt-cantidad").val(respuesta.Cantidad);
+                        $("#txt-precio").val(respuesta.precio);
+                        $("#txt-fechaElaboracion").val(respuesta.fecha_Elaboracion);
+                        $("#txt-fechaVencimiento").val(respuesta.fecha_Vencimiento);
+
                 },
                 error: function(){
-                    alert("Ocurrio un error");
+                    alert("Introduzca codigo de medicamento a buscar");
                 }
         });
 
 }
 
-
+    function EliminarMedicamento(){
+        var parametro = "txt-codigo="+$("#txt-busqueda").val();
+        $.ajax({
+            url:"../../ajax/procesar_farmacia.php?accion=2",
+            method:"POST",
+            data: parametro,
+            success: function(respuesta){
+                alert(respuesta);
+                
+            },
+            error:function(){
+                alert("ocurrio un error");
+            }
+        });
+     
+    }  
 
 
 
 
  $(document).ready(function(){
              $("#btn-actualizar").click(function(){
-                Validacion();
+                Validacion2();
              });
 
 
@@ -104,67 +200,52 @@ function busqueda(){
 
 
              $("#btn-eliminar").click(function(){
-                 $.ajax({
-                    url:"../../ajax/procesar_farmacia.php?accion=2",
-                    method:"POST",
-                    success:function(respuesta){
-                        alert(respuesta);
-                    },
-                     error:function(){
-                        alert("Ocurrio un error.");
-                    }
-                });
-                
+                EliminarMedicamento();                
              });
-             $("#btn-pagar").click(function(){
-                 $.ajax({
-                    url:"../../ajax/procesar_farmacia.php?accion=3",
-                    method:"POST",
-                    success:function(respuesta){
-                        alert(respuesta);
-                    },
-                     error:function(){
-                        alert("Ocurrio un error.");
-                    }
-                });
-                
-             });
+
              $("#btn-buscar").click(function(){
-            busqueda();
+                var codigo = $("#txt-busqueda").val();
+                busqueda(codigo);
     });
 
             
-            $("#btn-ir").click(function(){
-                 $.ajax({
-                    url:"../../ajax/procesar_farmacia.php?accion=4",
-                    method:"POST",
-                    success:function(respuesta){
-                        alert(respuesta);
-                    },
-                     error:function(){
-                        alert("Ocurrio un error.");
-                    }
-                });
+            $("#btn-cancelar").click(function(){
+                            $("#txt-nombre").val('');
+                            $("#txt-cantidad").val('');
+                            $("#txt-precio").val('');
+                            $("#txt-fechaElaboracion").val('');
+                            $("#txt-fechaVencimiento").val('');
+                            $("#txt-busqueda").val('');
                 
              });
+
+            $("#btn-crearPDF").click(function(){
+          
+            });
+
+
         CargarSucursales = function(){
         $.ajax({
-            url:"../../ajax/procesar_admin.php?accion=5",
+            url:"../../ajax/procesar_farmacia.php?accion=5",
             method:"POST",
             success: function(respuesta){
+            $("#div-sucursales").html(respuesta);
                 
             },
             error:function(){
 
             }
         });
-     
     }
+
+    CargarSucursales();
+
     CargarMedicamentos = function(){
         $.ajax({
-            url:"../../ajax/procesar_admin.php?accion=6",
+            url:"../../ajax/procesar_farmacia.php?accion=6",
             method:"POST",
             success: function(respuesta){
+                $("#div-medicamentos").html(respuesta);
                 
             },
             error:function(){
@@ -173,13 +254,51 @@ function busqueda(){
         });
      
     }
+
+    CargarMedicamentos();
+
+    CargarPagoMedicamentos = function(){
+        $.ajax({
+            url:"../../ajax/procesar_farmacia.php?accion=3",
+            method:"POST",
+            success: function(respuesta){
+                $("#div-Pagomedicamentos").html(respuesta);
+                
+            },
+            error:function(){
+
+            }
+        });     
+    }
+
+    CargarPagoMedicamentos();
+
+    CargarRecetas = function(){
+        $.ajax({
+            url:"../../ajax/procesar_farmacia.php?accion=4",
+            method:"POST",
+            success: function(respuesta){
+                $("#div-recetas").html(respuesta);
+                
+            },
+            error:function(){
+
+            }
+        });
+     
+    }
+        CargarRecetas();
+
+
+
 
     Registrar = function (){
                      var parametros ="txt-nombre= "+$("#txt-nombre").val()+"&"+
             "txt-cantidad="+$("#txt-cantidad").val()+"&"+
-            "txt-descripcion="+$("#txt-descripcion").val()+"&"+
+            "txt-sucursal="+$("#txt-sucursal").val()+"&"+
             "txt-fechaElaboracion="+$("#txt-fechaElaboracion").val()+"&"+
-            "txt-fechaVencimiento="+$("#txt-fechaVencimiento").val(); 
+            "txt-fechaVencimiento="+$("#txt-fechaVencimiento").val()
+            +"&"+"txt-precio="+$("#txt-precio").val();
 
                 $.ajax({
                     url:"../../ajax/procesar_farmacia.php?accion=8",
@@ -194,7 +313,34 @@ function busqueda(){
                 });
     }
 
+    Actualizar = function(){
+            var parametros ="txt-nombre= "+$("#txt-nombre").val()+"&"+
+            "txt-cantidad="+$("#txt-cantidad").val()+"&"+
+            "txt-sucursal="+$("#txt-sucursal").val()+"&"+
+            "txt-fechaElaboracion="+$("#txt-fechaElaboracion").val()+"&"+
+            "txt-fechaVencimiento="+$("#txt-fechaVencimiento").val()
+            +"&"+"txt-precio="+$("#txt-precio").val()+"&"+
+            "txt-codigo="+$("#txt-busqueda").val();
+
+                $.ajax({
+                    url:"../../ajax/procesar_farmacia.php?accion=9",
+                    method:"POST",
+                    data: parametros,
+                    success:function(respuesta){
+                        alert(respuesta);
+                    },
+                     error:function(){
+                        alert("Ocurrio un error.");
+                    }
+                });
+    }
+
+
+
 
         });
+
+
+
 
 
