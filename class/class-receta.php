df3a904d90c0cad698abed6e0d4b3dd06887dce1
alexisGomez101 <alexis.gomeznh@gmail.com>
2016-11-18 00:12:07 -0600

    Add files via upload

--- class/class-receta.php
+++ class/class-receta.php
@@ -1,40 +1,74 @@
 <?php
 
-	class Receta{
+class receta{
 
-		private $codigoReceta;
-		private $descripcion;
-		private $dosis;
+		private $nombre;
+		private $cantidad;
+		private $fecha;
+		private $precio;
 
-		public function __construct($codigoReceta,
-					$descripcion,
-					$dosis){
-			$this->codigoReceta = $codigoReceta;
-			$this->descripcion = $descripcion;
-			$this->dosis = $dosis;
+		public function __construct($nombre,
+					$cantidad,
+					$fecha,
+					$precio
+					){
+			$this->nombre = $nombre;
+			$this->cantidad = $cantidad;
+			$this->fecha = $fecha;
+			$this->precio = $precio;
 		}
-		public function getCodigoReceta(){
-			return $this->codigoReceta;
+		public function getNombre(){
+			return $this->nombre;
 		}
-		public function setCodigoReceta($codigoReceta){
-			$this->codigoReceta = $codigoReceta;
+		public function setNombre($nombre){
+			$this->nombre = $nombre;
 		}
-		public function getDescripcion(){
-			return $this->descripcion;
+		public function getCantidad(){
+			return $this->cantidad;
 		}
-		public function setDescripcion($descripcion){
-			$this->descripcion = $descripcion;
+		public function setCantidad($cantidad){
+			$this->cantidad = $cantidad;
 		}
-		public function getDosis(){
-			return $this->dosis;
+		public function getFecha(){
+			return $this->fecha;
 		}
-		public function setDosis($dosis){
-			$this->dosis = $dosis;
+		public function setFecha($fecha){
+			$this->fecha = $fecha;
 		}
+		public function getPrecio(){
+			return $this->precio;
+		}
+		public function setPrecio($precio){
+			$this->precio = $precio;
+		}
+		
 		public function toString(){
-			return "CodigoReceta: " . $this->codigoReceta . 
-				" Descripcion: " . $this->descripcion . 
-				" Dosis: " . $this->dosis;
+			return "Nombre: " . $this->nombre . 
+				" Cantidad: " . $this->cantidad . 
+				" Fecha: " . $this->fecha . 
+				" Precio: " . $this->precio;
 		}
+
+
+	public function guardarRegistroReceta($conexion){
+
+            $sql = sprintf(
+                "INSERT INTO tbl_medicinas(cod_medicamento, nombre, cantidad, fecha, precio)
+                VALUES (null, '%s', '%s', '%s', '%s')",
+                        stripslashes($this->nombre),
+                        stripslashes($this->cantidad),
+                        stripslashes($this->fecha),
+                        stripslashes($this->precio)
+                );
+        
+            //echo $sql;
+            $resultado = $conexion->ejecutarInstruccion($sql);
+            if ($resultado){
+                echo "Se Registro";
+            }
+            else{
+                echo "nada";
+            }
+        }
 	}
 ?>
