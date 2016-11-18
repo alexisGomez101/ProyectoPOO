f575eac1e1ce4a3c525e8d0a70dd315d8bd9c5f7
alexisGomez101 <alexis.gomeznh@gmail.com>
2016-11-18 00:13:47 -0600

    Add files via upload

--- ajax/procesar_doctores.php
+++ ajax/procesar_doctores.php
@@ -1,26 +1,57 @@
 <?php 
+  include_once("../class/class_conexion.php");
+include_once("../class/class-expediente.php");
+include_once("../class/class-receta.php");
 
 switch ($_GET["accion"]) {
 	case '1':
-	    include_once("../class/class_conexion.php");
-		include_once("../class/class_expediente.php");
+	include_once("../class/class_conexion.php");		
+		    
+            $conexion = new Conexion();
+          
+           
+            $resultado=$conexion->EjecutarInstruccion(
+            'SELECT a.id_expediente, a.sintomas,b.peso, b.altura, c.edad, c.fecha_visita
+			FROM tbl_expediente a
+			INNER JOIN tbl_preclinica b
+			INNER JOIN tbl_datospaciente c
+			On b.id_expediente='. $_POST["txt-codigo"]);//.$codigo);//.$_POST["txt-busqueda"]);
 
-		$conexion = new Conexion();
-       $registro = new registrar_medicamento(null,
-            $_POST["txt-peso"],$_POST["txt-altura"],$_POST["txt-temperatura"],$_POST["txt-sintomas"],$_POST["txt-fecha-cita"],$_POST["txt-hora-cita"]
-            );
+			$fila = $conexion->obtenerFila($resultado);
 
-		$peso = $_POST["txt-peso"];
-	    $numeroExpediente = $_POST["txt-noExpediente"];
-	    $fecha = $_POST["txt-fecha"];
-	    $peso =$_POST["txt-peso"];
-	    $edad =$_POST["txt-edad"];
-	    $descripcion =$_POST["txt-descripcion"];
+         ?>
+         <table class = "table table-striped table-hover">
+        	 <tr>
+	         	<td>Fecha:</td>
+	          	<td>
+	          		<input type="text" id="txt-fecha" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["fecha_visita"].'"';?> ></td>
+	          </tr>
+	   
+	          <tr>
+		          <td>Edad:</td>
+		          <td>
+	          	<input type="text" id="txt-edad" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["edad"].'"';?> ></td>
+	          </tr>
 
-	    
+	          <tr>
+		          <td>Peso:</td>
+		          <td>
+		          <input type="text" id="txt-peso" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["peso"].'"';?> ></td>
+	          </tr>
+	           <tr>
+		          <td>Sintomas:</td>
+		          <td>
+		          <input type="text" id="txt-sintomas" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["sintomas"].'"';?> ></td>
+	          </tr>
+	           <tr>
+		          <td>Altura:</td>
+		          <td>
+		          <input type="text" id="txt-altura" class="form-control" disabled="disabled" value=<?php echo '"'.$fila["altura"].'"';?> ></td>
+	          </tr>
+          
+          </table>
 
-	  $registro -> guardarRegistro($conexion);
-
+   <?php
 		break;
 
 	case '2':
@@ -36,7 +67,7 @@ switch ($_GET["accion"]) {
 			INNER JOIN tbl_datospaciente c
 			On b.id_expediente='.$_POST["txt-codigo"]);
 
-			$fila = $conexion->obtenerFila($resultado)
+			$fila = $conexion->obtenerFila($resultado);
 
          ?>
          <table class = "table table-striped table-hover">
@@ -66,29 +97,55 @@ switch ($_GET["accion"]) {
 		$peso = $_POST["txt-peso"];
 		$altura = $_POST["txt-altura"];
 		$temperatura = $_POST["txt-temperatura"];
-		$sintomas = $_POST["slc-sintomas"];
+		$sintomas = $_POST["txt-sintomas"];
 		$fecha = $_POST["txt-fecha-cita"];
 		$hora= $_POST["txt-hora-cita"];
 
-		if(strlen($peso)==0||strlen($altura==0||strlen($temperatura)==0){
-			if(strlen($sintomas)==0){
-				if(strlen($fecha)==0||strlen($hora)==0){
-					echo "Y";
-				}else{
-				echo "N";
-			}
-			}else {
-				echo "N";
-			}
+		if(strlen($peso)==0||strlen($altura)==0||strlen($temperatura)==0||strlen($sintomas)==0||strlen($fecha)==0||strlen($hora)==0){
+				echo "Y";
 		}else{
 			echo "N";
 		}
 
+	break;
 
-		break;
-		
+		case '4':
+		$conexion = new Conexion();
+		$registro = new expediente(null,$_POST["txt-peso"],$_POST["txt-altura"],$_POST["txt-temperatura"],$_POST["txt-sintomas"],$_POST["txt-hora-cita"], $_POST["txt-fecha-cita"]
+            );
+
+        $registro -> guardarRegistro($conexion);
+	break;
+
+	case '5':
+
+		$nombre = $_POST["txt-nombre"];
+		$cantidad = $_POST["txt-cantidad"];
+		$fecha = $_POST["txt-fecha"];
+		$precio = $_POST["txt-precio"];
+
+		if(strlen($nombre)==0||strlen($cantidad)==0||strlen($fecha)==0||strlen($precio)==0){
+				echo "Y";
+		}else{
+			echo "N";
+		}
+
+		break;	
+
+	case '6':
+		$conexion = new Conexion();
+		$registro = new receta(null,
+			$_POST["txt-nombre"],
+			$_POST["txt-cantidad"],
+			$_POST["txt-fecha"],
+			$_POST["txt-precio"]
+			);
+
+        $registro -> guardarRegistroReceta($conexion);
+	break;
 	
 	default:
+
 		echo "Error";
 		break;
 }
